a13e6ea9948ccdb031f57bfc1754c0cccda53933
Eugene <eugenefoh84@gmail.com>
2025-01-20 17:11:10 +0800

    update animation for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "framer-motion";
 import AboutSection from "./Home/AboutSection";
 import AchievementsSections from "./Home/AchievementsSection";
 import EmailSection from "./Home/EmailSection";
@@ -11,12 +13,43 @@ export default function Home() {
     <main className="flex flex-col bg-[#F4EBEF]">
       <NavBar />
       <div className="container mt-24 mx-auto px-12 py-8">
-        <HeroSection />
-        <AchievementsSections />
-        <AboutSection />
-        <ProjectSection />
+        {/* Hero Section with scroll animation */}
+        <motion.div 
+          initial={{ opacity: 0 }} 
+          whileInView={{ opacity: 1 }} 
+          transition={{ duration: 0.6 }}
+        >
+          <HeroSection />
+        </motion.div>
+
+        {/* Achievements Section with scroll animation */}
+        <motion.div 
+          initial={{ opacity: 0 }} 
+          whileInView={{ opacity: 1 }} 
+          transition={{ duration: 0.6 }}
+        >
+          <AchievementsSections />
+        </motion.div>
+
+        {/* About Section with scroll animation */}
+        <motion.div 
+          initial={{ opacity: 0 }} 
+          whileInView={{ opacity: 1 }} 
+          transition={{ duration: 0.6 }}
+        >
+          <AboutSection />
+        </motion.div>
+
+        {/* Project Section with scroll animation */}
+        <motion.div 
+          initial={{ opacity: 0 }} 
+          whileInView={{ opacity: 1 }} 
+          transition={{ duration: 0.6 }}
+        >
+          <ProjectSection />
+        </motion.div>
       </div>
-      <Footer/>
+      <Footer />
     </main>
   );
 }
